a12af019c8de0a8da069d314076884b5cb81321d
SkirdinNikita <skirdin.nik@gmail.com>
2020-02-20 19:52:53 +0300

    added navigation

--- src/app/src/Components/Header.js
+++ src/app/src/Components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Navigation from "./Navigation"
 
 class Header extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
     render() {
-        return <header><h1>типо LASTPASS</h1></header>
+        return <header>
+            <h1>типо LASTPASS</h1>
+            <Navigation navigationArr={this.props.navigationArr} />
+        </header>
     }
 }
 
