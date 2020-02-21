985c9261f91850fbacadd0178502e9a371743abe
SkirdinNikita <skirdin.nik@gmail.com>
2020-02-21 14:22:23 +0300

    updated

--- src/app/src/Components/Header.js
+++ src/app/src/Components/Header.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import Navigation from "./Navigation"
+import ReactDOM from 'react-dom';
+import AddPassword from './AddPassword';
 
 class Header extends React.Component {
-
+    constructor(props) {
+        super(props);
+        this.closeButtonHandler = this.closeButtonHandler.bind(this);
+        this._buttonAddPasswordHandler = this._buttonAddPasswordHandler.bind(this);
+    }
+    _buttonAddPasswordHandler() {
+        ReactDOM.render(<AddPassword closeButtonHandler={this.closeButtonHandler} />, document.querySelector('.wrapper'));
+    }
+    closeButtonHandler() {
+        ReactDOM.unmountComponentAtNode(document.querySelector('.wrapper'));
+    }
     render() {
         return <header>
             <h1>типо LASTPASS</h1>
-            <Navigation navigationArr={this.props.navigationArr} />
+            <button className="" onClick={this._buttonAddPasswordHandler}>Добавить новый пароль</button>
         </header>
     }
 }
